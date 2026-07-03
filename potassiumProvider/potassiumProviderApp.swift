93f6fd8ac9087ecb77ad795afde32d762d9646e1
OpenCow <opencow@ik.me>
2026-07-03 21:42:38 +0200

    feat: Initial project init

---
+++ potassiumProvider/potassiumProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  potassiumProviderApp.swift
+//  potassiumProvider
+//
+//  Created by OpenCow on 03/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct potassiumProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ potassiumProviderTests/potassiumProviderTests.swift
@@ -0,0 +1,18 @@
+//
+//  potassiumProviderTests.swift
+//  potassiumProviderTests
+//
+//  Created by OpenCow on 03/07/2026.
+//
+
+import Testing
+
+struct potassiumProviderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
